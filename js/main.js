cc071b96ae273d6c6646ba3f713b5a87b09580d4
Oleg <olegdanilovic19@gmail.com>
2021-10-21 01:46:55 +0300

    space3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
+    $('.slider__box').slick({
+        prevArrow: '<img src="images/arrow-left.svg" alt="arrow-left" class="slider__arrow slider__arrow-left">',
+        nextArrow: '<img src="images/arrow-right.svg" alt="arrow-right" class="slider__arrow slider__arrow-right">',
+    });
 
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('active');
+    });
 });
+
